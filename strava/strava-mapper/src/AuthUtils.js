ccc29007ce9d53bae12956f4befd8d8beb4822b4
kirkH <kirk@passivelogic.com>
2023-04-12 23:09:30 -0600

    .env

--- strava/strava-mapper/src/AuthUtils.js
+++ strava/strava-mapper/src/AuthUtils.js
@@ -2,7 +2,7 @@ function authenticate() {
     console.log(process.env.REACT_APP_CLIENT_ID);
     console.log(process.env.REACT_APP_CLIENT_SECRET);
     console.log(process.env.REACT_APP_ACCESS_TOKEN);
-    console.log(process.env.REACT_APP_REFRESH_TOkEN);
+    console.log(process.env.REACT_APP_REFRESH_TOKEN);
 }
 
 export { authenticate };
