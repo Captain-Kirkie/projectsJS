bdee0928e74622e74cc8fb62f0295f20b6fceea0
kirkH <kirk@passivelogic.com>
2023-04-12 23:08:10 -0600

    .env

--- strava/strava-mapper/src/App.js
+++ strava/strava-mapper/src/App.js
@@ -3,6 +3,7 @@ import * as d3 from "d3";
 import USAMap from "react-usa-map";
 import { AwesomeButton } from "react-awesome-button";
 import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
+import { authenticate } from "./AuthUtils";
 
 function App() {
     d3.selectAll("div").style("color", "blue");
@@ -15,6 +16,7 @@ function App() {
                 onPress={(event, release) => {
                     // do a sync/async task then call `release()`
                     console.log("Authenticate");
+                    authenticate();
                 }}
             >
                 Authenticate
@@ -24,7 +26,6 @@ function App() {
                 type="secondary"
                 onPress={(event, release) => {
                     // do a sync/async task then call `release()`
-                    console.log("Authenticate");
                 }}
             >
                 Refresh
--- strava/strava-mapper/src/AuthUtils.js
+++ strava/strava-mapper/src/AuthUtils.js
@@ -1,18 +1,8 @@
-import { AwesomeButton } from "react-awesome-button";
-import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
-
-function Button(props) {
-    return (
-        <AwesomeButton
-            cssModule={AwesomeButtonStyles}
-            type="primary"
-            onPress={(event, release) => {
-                // do a sync/async task then call `release()`
-                console.log("Authenticate");
-            }}
-            active={true}
-        ></AwesomeButton>
-    );
+function authenticate() {
+    console.log(process.env.REACT_APP_CLIENT_ID);
+    console.log(process.env.REACT_APP_CLIENT_SECRET);
+    console.log(process.env.REACT_APP_ACCESS_TOKEN);
+    console.log(process.env.REACT_APP_REFRESH_TOkEN);
 }
 
-export default Button;
+export { authenticate };
